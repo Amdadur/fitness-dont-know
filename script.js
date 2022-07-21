45a36bf4c1d337f14cd02b28dd387abf63d7808a
Amdadur <amdad@live.co.uk>
2022-07-21 13:46:54 +0100

    a start

---
+++ script.js
@@ -0,0 +1,10 @@
+const dontknow = document.querySelector(".dont-know")
+const age = document.querySelector(".age")
+const bodyfat = document.querySelector(".body-fat")
+const exp = document.querySelector(".exp")
+const filter = document.querySelector(".filter")
+const muscle = document.querySelector(".muscle")
+const recomp = document.querySelector(".recomp")
+const cutbulk = document.querySelector(".cut-bulk")
+
+dontknow.addEventListener("click", function(){ dontknow.style.display = "none";age.style.display = "block";  });
